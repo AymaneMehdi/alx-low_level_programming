ae202fc2d0d41d7a699e2baee0054ad3ec90bc47
Aymane Mehdi <unlocked.dark.web@gmail.com>
2023-09-28 15:18:53 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
 /**
  * binary_to_uint - convert a binary number to an unsigned int
  * @b: char string
- * Return: converted decimal number or 0 if there is an unconvertable char
+ * Return: converted decimal number or 0 if there is an unconvertible char
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -13,13 +13,14 @@ unsigned int binary_to_uint(const char *b)
     if (b == NULL)
         return (0);
 
-    while (*b)
+    for (int i = 0; b[i]; i++)
     {
-        if (*b != '0' && *b != '1')
+        if (b[i] != '0' && b[i] != '1')
             return (0);
 
-        total = (total << 1) | (*b - '0');
-        b++;
+        total <<= 1; // Shift left by 1 to make room for the next bit
+        if (b[i] == '1')
+            total |= 1; // Set the least significant bit to 1 if it's '1'
     }
 
     return (total);
